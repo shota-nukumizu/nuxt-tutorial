2e9e79e7f89f9a88533c2de4bd041a538fdb7598
shota-nukumizu <author2000.1225@gmail.com>
2022-01-15 11:14:51 +0900

    create AppHeader

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Welcome to My Sample Page.</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+    head() {
+        return {
+            title: 'Welcome to Sample Page',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Bast Place for sample Page'
+                }
+            ]
+        }
+    }
 }
 </script>
