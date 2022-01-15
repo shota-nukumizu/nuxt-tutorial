a3d7f21513979f52a745e43ca07b0b514ec06029
shota-nukumizu <author2000.1225@gmail.com>
2022-01-15 11:04:11 +0900

    create About page

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+       <h1>About Page</h1>
+       <p>This is a sample page made in Nuxt.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About App',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Bast Place for sample Page'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
